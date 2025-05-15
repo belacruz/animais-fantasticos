02c66b0d6ce5f57bf36d99884ad6625ab9b5b8d8
Felipe Bela Cruz <liphebc@gmail.com>
2025-05-15 12:11:06 -0300

    Atualização

--- js/modules/accordion.js
+++ js/modules/accordion.js
@@ -1,27 +1,18 @@
-export default class Accordion {
-  constructor(list) {
-    this.accordionList = document.querySelectorAll(list);
-    this.activeClass = 'ativo';
+export default function initAccordion() {
+  const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
+  const activeClass = 'ativo';
+
+  function activeAccordion() {
+    this.classList.toggle(activeClass);
+    this.nextElementSibling.classList.toggle(activeClass);
   }
 
-  toggleAccordion(item) {
-    item.classList.toggle(this.activeClass);
-    item.nextElementSibling.classList.toggle(this.activeClass);
-  }
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
 
-  // adiciona os eventos ao accordion
-  addAccordionEvent() {
-    this.accordionList.forEach((item) => {
-      item.addEventListener('click', () => this.toggleAccordion(item));
+    accordionList.forEach((item) => {
+      item.addEventListener('click', activeAccordion);
     });
-  }
-
-  // iniciar função
-  init() {
-    if (this.accordionList.length) {
-      // ativar primeiro item
-      this.toggleAccordion(this.accordionList[0]);
-      this.addAccordionEvent();
-    }
   }
 }
