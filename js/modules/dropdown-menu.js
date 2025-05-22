209b1790fa7e8c40628fcacfcf41e01a0d9dbd09
Felipe Bela Cruz <liphebc@gmail.com>
2025-05-22 14:03:28 -0300

    Dropdown-menu refatorado em classe

--- js/modules/dropdown-menu.js
+++ js/modules/dropdown-menu.js
@@ -1,19 +1,40 @@
-import outsideClick from './outsideclick.js';
+import outsideClick from "./outsideclick.js";
 
-export default function initDropdownMenu() {
-  const dropdownMenus = document.querySelectorAll('[data-dropdown]');
+export default class DropdownMenu {
+  constructor(menus, events) {
+    this.dropdownMenus = document.querySelectorAll(menus);
 
-  function handleClick(event) {
+    if (events === undefined) {
+      this.eventOptions = ["touchstart", "click"];
+    } else {
+      this.eventOptions = events;
+    }
+
+    this.activeClass = "active";
+    this.activeDropdownMenu = this.activeDropdownMenu.bind(this);
+  }
+
+  activeDropdownMenu(event) {
     event.preventDefault();
-    this.classList.add('active');
-    outsideClick(this, ['touchstart', 'click'], () => {
-      this.classList.remove('active');
+    const eventElement = event.currentTarget;
+    eventElement.classList.add(this.activeClass);
+    outsideClick(eventElement, this.eventOptions, () => {
+      eventElement.classList.remove(this.activeClass);
     });
   }
 
-  dropdownMenus.forEach((menu) => {
-    ['touchstart', 'click'].forEach((userEvent) => {
-      menu.addEventListener(userEvent, handleClick);
+  addDropDownMenusEvent() {
+    this.dropdownMenus.forEach((menu) => {
+      this.eventOptions.forEach((userEvent) => {
+        menu.addEventListener(userEvent, this.activeDropdownMenu);
+      });
     });
-  });
+  }
+
+  init() {
+    if (this.dropdownMenus.length) {
+      this.addDropDownMenusEvent();
+    }
+    return this;
+  }
 }
